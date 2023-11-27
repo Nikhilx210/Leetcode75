d00afc5bba8f557323ad12a80181046ec8de3f19
Nikhilx210 <nikhilrathee736@gmail.com>
2023-11-27 16:18:20 +0530

    solution

---
+++ q10.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class q10 {
+    public void moveZeroes(int[] nums) {
+        int len=nums.length;
+        if(len==1){
+            return;
+        }
+        int zst=0;
+        boolean z=true;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==0 && z==true){
+                zst=i;
+                z=false;
+            }
+            if(nums[i]!=0 && nums[i-1]==0){
+                int swap=nums[i];
+                nums[i]=nums[zst];
+                nums[zst]=swap;
+                zst++;
+            }
+            System.out.println(Arrays.toString(nums));
+        }
+    }
+}
